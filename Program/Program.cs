8f670d9a84c20d727ea76e2f4dad1842f7d38426
g81433416-netizen <g81433416@gmail.com>
2026-03-18 20:10:04 +0300

    Add user name input and greeting

--- Program/Program.cs
+++ Program/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите свое имя:");
+        string? name= Console.WriteLine($"Привет, {name}")
     }
 }
