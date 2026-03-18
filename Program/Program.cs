c17517c5875de3d70666cf39504e50a7bec58203
g81433416-netizen <g81433416@gmail.com>
2026-03-18 20:34:56 +0300

    new branch

--- Program/Program.cs
+++ Program/Program.cs
@@ -6,6 +6,7 @@ class Program
     {
         Console.WriteLine("Введите свое имя:");
         string? name= Console.WriteLine($"Здарова, {name}");
-        Console.WriteLine("Ты кто такой? Давай дасвиданья!")
+        Console.WriteLine("Ты кто такой? Давай дасвиданья!");
+        Console.WriteLine(1+1);
     }
 }
