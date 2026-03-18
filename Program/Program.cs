015e15b17bf94fd43211d60405201870f91adb06
g81433416-netizen <g81433416@gmail.com>
2026-03-18 20:19:25 +0300

    add line

--- Program/Program.cs
+++ Program/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Введите свое имя:");
-        string? name= Console.WriteLine($"Привет, {name}")
+        string? name= Console.WriteLine($"Здарова, {name}");
+        Console.WriteLine("Ты кто такой? Давай дасвиданья!")
     }
 }
